a3db926a70fc21c2f08e6a6d8997057440ed1eae
TornikePkhakadze <pheixa@gmail.com>
2025-01-23 14:28:25 +0400

    Delete masw_3.5_saati_bevria directory

--- masw_3.5_saati_bevria/videosharer/profiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProfileView, UpdateProfile
-
-urlpatterns = [
-    path('<int:pk>/', ProfileView.as_view(), name='profile'),
-
-    path('<int:pk>/update/', UpdateProfile.as_view(), name='update-profile'),
-]
